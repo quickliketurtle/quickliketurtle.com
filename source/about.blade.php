0772d3c0dd49b94a9954b0267b7df95c9396b39b
Jeff Finley <quickliketurtle@gmail.com>
2017-08-13 17:44:39 -0700

    change contact to about page

---
+++ source/about.blade.php
@@ -0,0 +1,13 @@
+@extends('_layouts.master')
+
+@section('content')
+    <div class="has-text-centered">
+        <h1 class="is-size-3">
+            About Me
+        </h1>
+    </div>
+    <div class="center-50">
+        <p>I do things and stuff</p>
+        <p>I also do stuff and things</p>
+    </div>
+@endsection
